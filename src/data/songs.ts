3c9d49854ae4eeabd948b9db8c9583275c59e52d
benitorhuang-svg <benitorhuang@gmail.com>
2026-04-14 21:04:37 +0800

    Cleanup: remove specific songs, keep architecture, and enable deployment on master branch

--- src/data/songs.ts
+++ src/data/songs.ts
@@ -1,7 +1,19 @@
 import type { Song } from "../types/song";
-import { pirates } from "./songs/pirates";
-import { starwars } from "./songs/starwars";
 
 export type { Song };
 
-export const songs: Song[] = [pirates, starwars];
+export const songs: Song[] = [
+  {
+    id: "demo-score",
+    title: "樂譜架構測試",
+    artist: "作曲家名稱",
+    notation: `X:1
+T:樂譜架構測試
+C:作曲家
+M:4/4
+L:1/4
+Q:1/4=120
+K:C
+| C D E F | G A B c | c B A G | F E D C |]`,
+  },
+];
